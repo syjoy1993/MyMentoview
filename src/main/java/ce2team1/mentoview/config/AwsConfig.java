22142ad5333a6d3d1647d3ca6e4b1177056e6eeb
linkpond <linkpond0629@gmail.com>
2025-03-03 14:57:15 +0900

    [MV-65] :zap: feat modify AwsConfig class, add AwsS3Service class

--- src/main/java/ce2team1/mentoview/config/AwsConfig.java
+++ src/main/java/ce2team1/mentoview/config/AwsConfig.java
@@ -2,12 +2,16 @@ package ce2team1.mentoview.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@Configuration
 public class AwsConfig {
+
     @Value("${cloud.aws.credentials.access-key}")
     private String awsAccessKey;
 
@@ -15,19 +19,20 @@ public class AwsConfig {
     private String awsSecretKey;
 
     @Value("${cloud.aws.region.static}")
-    private String region;
+    private String awsRegion;
 
     @Bean
-    public S3Client s3client() {
+    // access key, secret key를 사용하여 AwsBasicCredentials의 새 인스턴스 생성
+    public AwsCredentials basicAWSCredentials() {
+        return AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
+    }
 
-        // S3 사용 인증 객체
-        //AWSCredentials  credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
-
-        // 리전 정보 입력 -> S3 사용 객체 생성
+    @Bean
+    public S3Client s3Client(AwsCredentials awsCredentials) {
         return S3Client.builder()
-                .region(Region.of(region)) //AWS 리전 설정
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(awsAccessKey, awsSecretKey))) // AWS 자격증명 설정
+                .region(Region.of(awsRegion)) //AWS 리전 설정
+                // 파라미터로 전달된 awsCredentials를 사용하여 자격증명공급자 설정
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
     }
 }
