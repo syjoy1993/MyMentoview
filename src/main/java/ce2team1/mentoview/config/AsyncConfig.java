e6f046e9375f4f814da441f9c6d138f7d21c5c14
CJ-Park <king01286@naver.com>
2025-03-12 10:54:43 +0900

    [MV-129] :zap: feat: 이미지 기반 PDF 텍스트 추출 시 비동기 처리로 수정 및 코드 레벨 성능 개선

--- src/main/java/ce2team1/mentoview/config/AsyncConfig.java
+++ src/main/java/ce2team1/mentoview/config/AsyncConfig.java
@@ -14,9 +14,9 @@ public class AsyncConfig {
     @Bean(name = "generateFeedbackExecutor")
     public Executor generateFeedbackExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(20);   // 최소 스레드 개수 (기본값)
-        executor.setMaxPoolSize(50);   // 최대 스레드 개수 (테스트 후 조정 가능)
-        executor.setQueueCapacity(200); // 대기 큐 크기 (이후 요청을 저장)
+        executor.setCorePoolSize(10);   // 최소 스레드 개수 (기본값)
+        executor.setMaxPoolSize(40);   // 최대 스레드 개수 (테스트 후 조정 가능)
+        executor.setQueueCapacity(100); // 대기 큐 크기 (이후 요청을 저장)
         executor.setThreadNamePrefix("Feedback-");
         executor.initialize();
         return executor;
