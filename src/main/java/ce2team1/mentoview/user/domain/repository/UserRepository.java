413fe9d16e3c48a6117267853a93272f12d357c8
saya 🌱 <syjoy1993@gmail.com>
2025-12-04 01:27:36 +0900

    add @

--- src/main/java/ce2team1/mentoview/user/domain/repository/UserRepository.java
+++ src/main/java/ce2team1/mentoview/user/domain/repository/UserRepository.java
@@ -4,10 +4,12 @@ package ce2team1.mentoview.user.domain.repository;
 import ce2team1.mentoview.user.domain.entity.User;
 import ce2team1.mentoview.user.domain.entity.atrribute.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
